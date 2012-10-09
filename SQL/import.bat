3a6c8bc5754893755dcaba36d9ec1cb922838260
ConanHUN <conan_513@hotmail.com>
2012-10-09 06:51:37 +0200

    JRE7 and preconfigured jar file added

---
+++ SQL/import.bat
@@ -0,0 +1,6 @@
+mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=moopledev < "MoopleDEV.sql"
+mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=moopledev < "storages.sql"
+mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=moopledev < "MonsterDrops.sql"
+mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=moopledev < "MiuMiu.sql"
+pause
+exit
